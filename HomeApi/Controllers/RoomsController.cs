9644d2c6c8a17e7fb35687a15ffe63cadcc49ef1
Djaba00 <shipelenko9@gmail.com>
2023-07-20 17:03:41 +0600

    initial commit

---
+++ HomeApi/Controllers/RoomsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using HomeApi.Contracts.Models.Rooms;
+using HomeApi.Data.Models;
+using HomeApi.Data.Queries;
+using HomeApi.Data.Repos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeApi.Controllers
+{
+    /// <summary>
+    /// Контроллер комнат
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class RoomsController : ControllerBase
+    {
+        private IRoomRepository _repository;
+        private IMapper _mapper;
+        
+        public RoomsController(IRoomRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        
+        //TODO: Задание - добавить метод на получение всех существующих комнат
+        
+        /// <summary>
+        /// Добавление комнаты
+        /// </summary>
+        [HttpPost] 
+        [Route("")] 
+        public async Task<IActionResult> Add([FromBody] AddRoomRequest request)
+        {
+            var existingRoom = await _repository.GetRoomByName(request.Name);
+            if (existingRoom == null)
+            {
+                var newRoom = _mapper.Map<AddRoomRequest, Room>(request);
+                await _repository.AddRoom(newRoom);
+                return StatusCode(201, $"Комната {request.Name} добавлена!");
+            }
+            
+            return StatusCode(409, $"Ошибка: Комната {request.Name} уже существует.");
+        }
+
+        /// <summary>
+        /// Изменение комнаты
+        /// </summary>
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Edit(
+            [FromRoute] Guid id,
+            [FromBody] EditRoomRequest request)
+        {
+            var room = await _repository.GetRoomById(id);
+
+            if(room == null)
+                return StatusCode(400, $"Ошибка: Комната с идентификатором {id} не подключена!");
+
+            await _repository.UpdateRoom(
+                room,
+                new UpdateRoomQuery(request.NewName, request.NewArea, request.NewGasConnected, request.NewVoltage)
+                );
+
+            return StatusCode(200, $"Комната обнавлена! {room.Name}. Площадь - {room.Area}, Газ - {room.GasConnected}, Вольтаж - {room.Voltage}");
+        }
+    }
+}
